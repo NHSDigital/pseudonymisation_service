da3754ad1ef6e4f478af7a5c9ac94815a14c3101
josh.pencheon <josh.pencheon@153ec483-9fa4-4787-9925-b21d4235b228>
2019-12-12 11:37:13 +0000

    # avoid rails wrapping parameters
    git-svn-id: https://deepthought/svn/non-era/pseudonymisation_service@20813 153ec483-9fa4-4787-9925-b21d4235b228

--- config/initializers/wrap_parameters.rb
+++ config/initializers/wrap_parameters.rb
@@ -1,11 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-# This file contains settings for ActionController::ParamsWrapper which
-# is enabled by default.
-
-# Enable parameter wrapping for JSON. You can disable this by setting :format to an empty array.
+# Disable parameter wrapping for JSON. You can enable this by setting :format to [:json].
 ActiveSupport.on_load(:action_controller) do
-  wrap_parameters format: [:json]
+  wrap_parameters format: []
 end
 
 # To enable root element in JSON for ActiveRecord objects.
