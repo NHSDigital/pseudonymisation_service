cfcd64fbe7d73b4a4fb98516985ed24533276b5e
josh.pencheon <josh.pencheon@153ec483-9fa4-4787-9925-b21d4235b228>
2020-02-14 16:07:49 +0000

    # log user separately, rather than inside auth callback
    git-svn-id: https://deepthought/svn/non-era/pseudonymisation_service@21541 153ec483-9fa4-4787-9925-b21d4235b228

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::API
   include ActionController::HttpAuthentication::Token::ControllerMethods
 
   before_action :authenticate
+  before_action :log_user
 
   # Any failure to persist is the fault of the server, rather than the client;
   # from the client's point of view, the server is stateless.
@@ -19,16 +20,18 @@ class ApplicationController < ActionController::API
   def authenticate
     authenticate_or_request_with_http_token do |token, _options|
       @current_user = User.retrieve_by(token: token)
-
-      if @current_user
-        Rails.logger.info("processing request for user: #{@current_user.try(:username)}")
-      else
-        Rails.logger.info('processing request for unauthenticated user')
-      end
     end
   end
 
   def current_user
     @current_user ||= authenticate
+  end
+
+  def log_user
+    if current_user
+      Rails.logger.info("processing request for user: #{current_user.try(:username)}")
+    else
+      Rails.logger.info('processing request for unauthenticated user')
+    end
   end
 end
