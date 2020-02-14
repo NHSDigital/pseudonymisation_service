5a54ca55d4b3b884b3ac8149af34146408f270a2
josh.pencheon <josh.pencheon@153ec483-9fa4-4787-9925-b21d4235b228>
2020-02-14 15:51:40 +0000

    # avoid loop possibility
    git-svn-id: https://deepthought/svn/non-era/pseudonymisation_service@21540 153ec483-9fa4-4787-9925-b21d4235b228

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,8 +20,8 @@ class ApplicationController < ActionController::API
     authenticate_or_request_with_http_token do |token, _options|
       @current_user = User.retrieve_by(token: token)
 
-      if current_user
-        Rails.logger.info("processing request for user: #{current_user.try(:username)}")
+      if @current_user
+        Rails.logger.info("processing request for user: #{@current_user.try(:username)}")
       else
         Rails.logger.info('processing request for unauthenticated user')
       end
